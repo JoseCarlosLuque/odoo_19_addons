a8de195b13a20c1649cf35363ea47f03ca651523
José Carlos Luque Castro <157233434+JoseCarlosLuque@users.noreply.github.com>
2026-06-06 01:44:35 +0200

    [FIX] cd_custom_task : Fix the problem in project_task_views

--- cd_custom_task/models/project_task.py
+++ cd_custom_task/models/project_task.py
@@ -5,7 +5,6 @@ class ProjectTask(models.Model):
     _inherit = 'project.task'
     _description = 'project task'
 
-
     crm_lead_id = fields.Many2one(
         'crm.lead',
         string='Tarea del lead',
